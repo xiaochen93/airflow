e726ec9f0546c6ec79d36683fc72435b968d1668
xiaochen93 <xiaochen.leo93@gmail.com>
2023-08-03 21:07:26 +0800

    2023-08-3 dag config

--- dags/berita_24hr_web_dag.py
+++ dags/berita_24hr_web_dag.py
@@ -49,7 +49,7 @@ exe_web_crawlers_dag = DAG('BM_berita_24hr_web_dag_v2',
 		default_args=default_args,
 		description='The dag object to execute a series of web crawlers for data/comments collection .',
 		schedule_interval= '0 12 * * *', #schedule interval to execute the task '* * * * *' '0 */12 * * *'
-		catchup=False,
+		catchup=True,
 		tags=['berita mediacorp','news articles','24hrs']
 )
 
