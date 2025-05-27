b1f8710ff1ad7dc9a2a2522a667aa934bc5f7d8d
xiaochen93 <xiaochen.leo93@gmail.com>
2025-05-27 10:03:40 +0800

    2025-05-27 update reddit_crawler to new architecture

--- src/news_comments_crawlers/others/data_migration.py
+++ src/news_comments_crawlers/others/data_migration.py
@@ -96,7 +96,10 @@ def migrate_data(limit=500):
         article_datetime = datetime.strptime((each_article["published_datetime_old"].split("."))[0], "%Y-%m-%dT%H:%M:%S")
         current_datetime = datetime.strptime(CURRENT_DATETIME, "%Y-%m-%d %H:%M:%S")
         #print(f"\n-- DEBUG: source id {each_article['source_id']} and date difference {(current_datetime - article_datetime).days}")
-        _is_from_forum = int(each_article['source_id']) in [5,16,17,19]
+        try:
+            _is_from_forum = int(each_article['source_id']) in [5,16,17,19]
+        except:
+            continue
         _is_within_time_range = (current_datetime - article_datetime).days <= 3
         
         if _is_from_forum and _is_within_time_range:
